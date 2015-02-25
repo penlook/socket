2efd6b2078f15ba8ce4ee94e24ef3ed2c8045353
loint <loint@penlook.com>
2015-02-26 00:54:50 +0700

    Add test case for client

--- client_test.go
+++ client_test.go
@@ -30,9 +30,117 @@ package socket
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"container/list"
+	//"fmt"
 )
 
-func TestClient(t *testing.T) {
+func createClient() (Client, string) {
+
+	socket := Socket {
+		Port : 3000,
+		Interval: 60,
+	}
+
+	handshake := random()
+	output    := make(chan Json, 10)
+	channel   := make(chan Context, 10)
+	event     := list.New()
+
+	client := Client {
+		Socket: socket,
+		Context: nil,
+		Output : output,
+		Channel: channel,
+		Event: event,
+		Handshake: handshake,
+		HandshakeFlag: false,
+		MaxEvent: 0,
+	}
+
+	return client, handshake
+}
+
+func TestClientOn(t *testing.T) {
+
 	assert := assert.New(t)
-	assert.Equal("test", "test")
+	client, _ := createClient()
+
+	assert.NotNil(client)
+
+	client.On("test", func(data Json) {
+	})
+
+	client.On("abc", func(data Json) {
+	})
+
+	assert.Equal(2, client.MaxEvent)
+	assert.Equal(2, client.Event.Len())
+}
+
+func TestClientEmit(t *testing.T) {
+
+	assert := assert.New(t)
+	client, _ := createClient()
+
+	assert.NotNil(client)
+
+	client.Emit("test", Json {
+		"key1" : "value1",
+		"key2" : "value2",
+	})
+
+	client.Emit("abc", Json {
+		"key1" : "value1",
+		"key2" : "value2",
+	})
+
+	assert.Equal(Json {
+		"event" : "test",
+		"data"	: Json {
+			"key1" : "value1",
+			"key2" : "value2",
+		},
+	}, <- client.Output)
+
+	assert.Equal(Json {
+		"event" : "abc",
+		"data"	: Json {
+			"key1" : "value1",
+			"key2" : "value2",
+		},
+	}, <- client.Output)
+
+}
+
+func TestClientBroadcast(t *testing.T) {
+
+	assert := assert.New(t)
+	client, _ := createClient()
+
+	assert.NotNil(client)
+	client.Socket.Initialize()
+	assert.NotNil(client.Socket.Clients)
+	assert.Equal(0, len(client.Socket.Clients))
+
+	for i := 0; i < 10000; i++ {
+		client_, handshake := createClient()
+		client.Socket.Clients[handshake] = client_
+	}
+
+	assert.Equal(10000, len(client.Socket.Clients))
+
+	client.Broadcast("test", Json {
+		"key1" : "value1",
+		"key2" : "value2",
+	})
+
+	for _, client_ := range client.Socket.Clients {
+		assert.Equal(Json {
+			"event" : "test",
+			"data"  : Json {
+				"key1" : "value1",
+				"key2" : "value2",
+			},
+		}, <- client_.Output)
+	}
 }
