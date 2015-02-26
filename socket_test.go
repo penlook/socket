54963736d1986da720f37a34809b97244e474393
loint <loint@penlook.com>
2015-02-26 23:39:40 +0700

    Removed sample code from test file

--- socket_test.go
+++ socket_test.go
@@ -259,91 +259,9 @@ func TestSocketClientServer(t *testing.T) {
 
 	assert.NotNil(response)
 
+	// Testing ...
+
 	fmt.Println()
-
-
-	/*
-	socket := Socket {
-		Port: 3000,
-		Token: "acbz@3345123124567",
-		Interval: 60,
-	}
-
-	socket.Initialize()
-	socket.Static("/static", "./example")
-
-	socket.Template("example")
-	socket.Router.GET("/", func(context *gin.Context) {
-		context.HTML(200, "index.html", Json {})
-	})
-
-	socket.On("connection", func(client Client) {
-		client.On("init", func(data Json) {
-
-			client.Broadcast("test", Json {
-				"eventdata" : "Broadcast",
-			})
-
-			client.On("test2", func(data Json) {
-				client.Emit("test2", Json {
-					"key" : "Package 2 from server",
-				})
-			})
-		})
-		client.Emit("test_on", Json {
-			"key" : "abcxyz",
-		})
-		client.On("test", func(data Json) {
-			client.Emit("test", Json {
-				"abce" : "Hello",
-				"abcf" : "Yes",
-			})
-
-			client.On("test_2", func(data Json) {
-				client.Emit("abc", Json {
-					"test" : "1234",
-				})
-				client.On("test_3", func(data Json) {
-					client.On("test_4", func(data Json) {
-						client.Emit("abc", Json {
-							"test" : "1234",
-						})
-					})
-					client.Emit("abc", Json {
-						"test" : "test",
-					})
-				})
-				client.On("test_5", func(data Json) {
-					client.Emit("abc", Json {
-						"abc3" : "234556",
-					})
-					client.On("test7", func(data Json) {
-						client.On("test8", func(data Json) {
-							client.Emit("abc", Json {
-								"abc" : "avc",
-							})
-						})
-						client.On("test9", func(data Json) {
-							client.Emit("abc", Json {
-								"abc" : "avc",
-							})
-						})
-					})
-				})
-				client.Emit("abc", Json {
-					"abc" : "abc",
-				})
-			})
-			client.On("test6", func(data Json) {
-				client.Emit("abcer", Json {
-					"abc" : "1245667",
-				})
-			})
-		})
-	})
-
-	socket.Listen()
-	*/
 }
 
 
