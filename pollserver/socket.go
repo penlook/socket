a02b61ad72d609d79a63a7d526d0078eafcd527f
loint <loint@penlook.com>
2015-03-04 06:33:21 +0700

    removed daemon from polling server

--- pollserver/socket.go
+++ pollserver/socket.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	daemon "github.com/penlook/daemon"
 	. "github.com/penlook/socket"
 )
 
 func main() {
-	daemon.Service {
-		Name: "socket",
-		Description: "Socket service",
-		Process: Service,
-		Port: 1234,
-	}.Initialize()
-}
-
-func Service(server daemon.Service) {
 
 	socket := Socket {
-		Port: server.Port,
+		Port: 1234,
 		Token: "acbz@3345123124567",
 		Interval: 10,
 	}
